afafbf895e80764c426766eb4ababf37209b6aa8
lizheng <zheng.li@flexiv.com>
2024-02-19 11:24:27 +0800

    #null

---
+++ src/store/modules/async-router.js
@@ -0,0 +1,34 @@
+/**
+ * 向后端请求用户的菜单，动态生成路由
+ */
+import { constantRouterMap } from '@/config/router.config'
+import { generatorDynamicRouter } from '@/router/generator-routers'
+
+const permission = {
+  state: {
+    routers: constantRouterMap,
+    addRouters: []
+  },
+  mutations: {
+    SET_ROUTERS: (state, routers) => {
+      state.addRouters = routers
+      state.routers = constantRouterMap.concat(routers)
+    }
+  },
+  actions: {
+    // eslint-disable-next-line space-before-function-paren
+    GenerateRoutes({ commit }, data) {
+      console.log('leez111', data)
+      return new Promise(resolve => {
+        const { token } = data
+        console.log(token, 'leez222')
+        generatorDynamicRouter(token).then(routers => {
+          commit('SET_ROUTERS', routers)
+          resolve()
+        })
+      })
+    }
+  }
+}
+
+export default permission
